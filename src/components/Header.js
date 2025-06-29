c8627902d5d4ce7af7048203cf4a83cdb05e7b8c
jaspreetsinghlearn <jaspreet.singh.learn@gmail.com>
2025-06-29 23:47:24 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => document.body.removeChild(script);
+  }, []);
+
   return (
     <header className="container-fluid bg-light py-3">
       <nav className="navbar navbar-expand-lg navbar-light container">
         <Link className="navbar-brand text-primary font-weight-bold" to="/">ITBrand</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBar">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navBar">
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
@@ -23,7 +39,7 @@ function Header() {
               <Link className="nav-link" to="/user">User Info</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/404">Not Found</Link> {/* optional for demo */}
+              <Link className="nav-link" to="/404">Not Found</Link>
             </li>
           </ul>
         </div>
